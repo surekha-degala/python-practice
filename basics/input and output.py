b4c1f886a7e895ced8bee1442648d609875dd85a
surekha-degala <surekhadegala22@gmail.com>
2025-09-10 18:28:54 +0530

    Create input and output.py

---
+++ basics/input and output.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+a = input()
+b = input()
+separator = input()[0]
+
+########### Write your code below ###############
+
+# Print with space
+print (a, b,sep = " ")
+# Print without newline at the end
+print(a,b, end = "")
+# Print with separator
+print (a, b, sep = separator)
+# Print without space
+print (a+b)
+########### Write your code above ###############
+
+
+#User function Template for python3
+a = input()
+b = input()
+c = input()
+# Write your code below that prints a a times and b b times, seperated by c
+print(str(a)*a +c+str(b))
