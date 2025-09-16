391664dc7da54fc6f1b1a7541366e13abf2f1018
surekha-degala <surekhadegala22@gmail.com>
2025-09-16 16:35:15 +0530

    Create reverse list.py

---
+++ basics/List/reverse list.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+def listTraversalReverse(arr):
+    #Write your code below
+    for i in range (len(arr)-1, -1, -1):
+        print(arr[i], end = " ")
+    
+    
+    #Don't add a new line as it is provided by the driver code
