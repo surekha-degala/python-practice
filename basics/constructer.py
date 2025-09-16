ac1c5ec94fc9f9c8baecd4e01601aa95b0c8eed9
surekha-degala <surekhadegala22@gmail.com>
2025-09-17 00:45:55 +0530

    Create constructer.py

---
+++ basics/constructer.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+# Driver code will print instance variable like this
+# user = User()
+# print(user.name)
+class User:
+    def __init__(self):
+        self.name ="Default"
