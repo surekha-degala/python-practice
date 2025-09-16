5f1f7f3dbb61e34e6f0b4b8bbc13824e32f626e6
surekha-degala <surekhadegala22@gmail.com>
2025-09-17 00:43:43 +0530

    Create design a class.py

---
+++ basics/design a class.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+# Design a class as described in the question
+# Code here
+class MyClass:
+    def display(self):
+        print("Hello World")
+
+# The call to display function is made like this
+# obj = MyClass()
+# obj.display()
