f903e5a1b0ad1942cfe81312e5924b3ce35c986f
surekha-degala <surekhadegala22@gmail.com>
2025-09-19 00:00:13 +0530

    Update counting articles.py

--- Leetcode/counting articles.py
+++ Leetcode/counting articles.py
@@ -1,1 +1,12 @@
 
+def count_articles(s):
+    s = s.lower() 
+  words = s.split(" ")
+  count = 0
+    for word in words:
+       if word == "a" or word == "an" or word == "the" or word == "a," or word == "an," or word == "the.":
+            count+=1
+          return count
+s = input()
+print(count_articles(s))
+    
