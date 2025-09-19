62594bb4fc9a06be2d9cebc5b961a885d13e4068
surekha-degala <surekhadegala22@gmail.com>
2025-09-19 22:59:43 +0530

    Create add digits.py

---
+++ Leetcode/math/add digits.py
@@ -0,0 +1,12 @@
+def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num >= 10:
+            sum = 0
+            while num >0:
+                sum += num %10
+                num //= 10
+            num = sum
+        return num
