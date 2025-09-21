1836a4f88c4ddc8d4d6958f148bb7f8051904dcd
surekha-degala <surekhadegala22@gmail.com>
2025-09-21 22:52:06 +0530

    Create palindrome.py

---
+++ Leetcode/math/palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x <0:
+            return False
+        num = x
+        rev = 0
+        while (num != 0):
+            rev = rev*10 + num %10
+            num = num //10
+        return rev == x
