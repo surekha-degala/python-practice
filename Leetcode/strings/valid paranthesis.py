acb9b495a3f1d1146add2987ef62c2862b2ff375
surekha-degala <surekhadegala22@gmail.com>
2025-09-23 13:23:49 +0530

    Create valid paranthesis.py

---
+++ Leetcode/strings/valid paranthesis.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')':'(','}':'{',']':'['}
+
+        for ch in s:
+            if ch in pairs.values():
+                stack.append(ch)
+            elif ch in pairs:
+                if not stack or stack.pop() != pairs[ch]:
+                    return False
+            else:
+                return False
+        return not stack
